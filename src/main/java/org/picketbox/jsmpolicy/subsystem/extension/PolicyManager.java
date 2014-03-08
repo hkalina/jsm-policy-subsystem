2b314b1db341e838cd3bd29e779ddd0ba5e14711
Jan Kalina <xkalin03@stud.fit.vutbr.cz>
2014-03-08 19:46:16 +0100

    Policy file validation

--- src/main/java/org/picketbox/jsmpolicy/subsystem/extension/PolicyManager.java
+++ src/main/java/org/picketbox/jsmpolicy/subsystem/extension/PolicyManager.java
@@ -1,16 +1,22 @@
 package org.picketbox.jsmpolicy.subsystem.extension;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.security.Policy;
 
 import org.jboss.as.controller.OperationFailedException;
 import org.jboss.logging.Logger;
 
+import sun.security.provider.PolicyParser;
+
 /**
  * Work with security manager and security policy files
  */
+@SuppressWarnings("restriction")
 public class PolicyManager {
 
 	public static final PolicyManager INSTANCE = new PolicyManager();
@@ -22,7 +28,7 @@ public class PolicyManager {
 	/**
 	 * Set policy file used on this JVM
 	 * @param fileContent Content of policy file to use
-	 * @throws IOException When creating of temporary file fails
+	 * @throws OperationFailedException
 	 */
 	public void setPolicyFile(String fileContent) throws OperationFailedException {
 
@@ -51,49 +57,40 @@ public class PolicyManager {
 	/**
 	 * Set policy file used on this JVM
 	 * @param policy URL of policy file (null means disable JSM)
+	 * @throws OperationFailedException
 	 */
-	public void setPolicy(String policy){
-
+	public void setPolicy(String policy) throws OperationFailedException {
         System.err.println("setPolicy("+policy+")");
+        if(policy==null){
+            System.setSecurityManager(null);
+        }else{
+            validatePolicyFile(policy);
+            System.err.println("Validation OK");
 
-        printStatus();
-
-		if(policy==null){ // disable JSM
-
-			log.info("JsmPolicy: unsetting policy");
-			System.setSecurityManager(null);
-
-		}else{ // enable JSM with specified policy file
-
-			try{
-
-				if(!new File(policy).canRead()){
-					log.error("JsmPolicy: policy file \""+policy+"\" cannot be readed!");
-					return; // Exception?
-				}
-
-				log.info("JsmPolicy: setting policy \""+policy+"\"");
-
-				System.setProperty("java.security.policy", policy);
-
-				//Policy.setPolicy(new JsmPolicy(Policy.getPolicy()));
-				Policy.getPolicy().refresh();
-
-				System.setSecurityManager(new SecurityManager());
-
-				log.info("JsmPolicy: policy set successfuly");
-
-			}catch(Exception e){
-				log.error("JsmPolicy: setting policy failed: "+e.toString());
-			}
-
-		}
-
-		printStatus();
-
+            System.setProperty("java.security.policy", policy);
+            Policy.getPolicy().refresh();
+            System.setSecurityManager(new SecurityManager());
+        }
 	}
 
-	public void refreshPolicy(){
+	public void validatePolicyFile(String path) throws OperationFailedException {
+	    try{
+	        InputStreamReader isr = new InputStreamReader(new FileInputStream(path));
+	        PolicyParser pp = new PolicyParser(true);
+	        pp.read(isr);
+	    }
+	    catch(PolicyParser.ParsingException e){
+	        throw new OperationFailedException("Parsing exception when parsing policy file from DMR: "+e.getLocalizedMessage());
+	    }
+	    catch(FileNotFoundException e){
+	        throw new OperationFailedException("Temporary policy file not found: "+e.getLocalizedMessage());
+	    }
+	    catch(IOException e){
+	        throw new OperationFailedException("IOException when validating policy file: "+e.getLocalizedMessage());
+        }
+	}
+
+	public void refreshDelegatingPolicy(){
 		try{
 			Class.forName("org.jboss.security.jacc.DelegatingPolicy"); // catch not existing JACC
 			if(Policy.getPolicy() instanceof org.jboss.security.jacc.DelegatingPolicy){
