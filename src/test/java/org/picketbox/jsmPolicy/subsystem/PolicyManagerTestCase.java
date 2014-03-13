451b630a9f441d7665cfa0010194e614f01513f0
Jan Kalina <xkalin03@stud.fit.vutbr.cz>
2014-03-13 19:44:07 +0100

    Better PolicyManagerTestCase

--- src/test/java/org/picketbox/jsmPolicy/subsystem/PolicyManagerTestCase.java
+++ src/test/java/org/picketbox/jsmPolicy/subsystem/PolicyManagerTestCase.java
@@ -13,7 +13,9 @@ import org.picketbox.jsmpolicy.subsystem.extension.PolicyManager;
 
 public class PolicyManagerTestCase {
 
-    private File testingFile = new File("/etc/passwd");
+    // two different files for testing file readability
+    private File testingFile1 = new File("/etc/passwd");
+    private File testingFile2 = new File("/etc/group");
 
     // Permission which PolicyManager has to have for switching
     private String requiredPermissions =
@@ -24,89 +26,97 @@ public class PolicyManagerTestCase {
             "    permission java.util.PropertyPermission \"java.security.policy\",\"read,write\";\n" +
             "    permission java.io.FilePermission \"${jboss.server.temp.dir}/-\",\"read,write,delete\";\n";
 
-    private String minimalPolicy =
+    private String policyMinimal =
             "grant {\n" + requiredPermissions +
             "};";
 
-    private String testingFileAllowingPolicy =
+    private String policyAllowingTestingFile1 =
             "grant {\n" + requiredPermissions +
-            "    permission java.io.FilePermission \""+testingFile.getAbsolutePath()+"\",\"read\";\n" +
+            "    permission java.io.FilePermission \""+testingFile1.getAbsolutePath()+"\",\"read\";\n" +
+            "};";
+
+    private String policyAllowingTestingFile2 =
+            "grant {\n" + requiredPermissions +
+            "    permission java.io.FilePermission \""+testingFile2.getAbsolutePath()+"\",\"read\";\n" +
             "};";
 
 
     @Before
     public void initPolicyManagerTests(){
-        // ${jboss.server.temp.dir} := /tmp
+        // ${jboss.server.temp.dir} := /tmp (directory for temporary policy files)
         System.setProperty("jboss.server.temp.dir",System.getProperty("java.io.tmpdir"));
     }
 
     @Test
-    public void testSetUnset() throws Exception {
+    public void testSetUnsetSecurityManager() throws Exception {
 
-        assertTrue(isTestingFileReadable());
+        assertTrue(isFileReadable(testingFile1));
         assertNull(System.getSecurityManager());
 
-        PolicyManager.INSTANCE.setPolicyFile(minimalPolicy);
+        PolicyManager.INSTANCE.setPolicyFile(policyMinimal);
 
-        assertFalse(isTestingFileReadable());
+        assertFalse(isFileReadable(testingFile1));
         assertNotNull(System.getSecurityManager());
         assertTrue(System.getSecurityManager() instanceof java.lang.SecurityManager);
 
         PolicyManager.INSTANCE.setPolicyFile(null);
 
-        assertTrue(isTestingFileReadable());
+        assertTrue(isFileReadable(testingFile1));
         assertNull(System.getSecurityManager());
 
     }
 
     @Test
-    public void testDifferentPoliciesFromMinimal() throws Exception {
+    public void testSetUnsetOnePolicy() throws Exception {
 
-        assertTrue(isTestingFileReadable());
+        assertTrue(isFileReadable(testingFile1));
+        assertTrue(isFileReadable(testingFile2));
 
-        PolicyManager.INSTANCE.setPolicyFile(minimalPolicy);
+        PolicyManager.INSTANCE.setPolicyFile(policyAllowingTestingFile1);
 
-        assertFalse(isTestingFileReadable());
-
-        PolicyManager.INSTANCE.setPolicyFile(testingFileAllowingPolicy);
-
-        assertTrue(isTestingFileReadable());
+        assertTrue(isFileReadable(testingFile1));
+        assertFalse(isFileReadable(testingFile2));
 
         PolicyManager.INSTANCE.setPolicyFile(null);
 
-        assertTrue(isTestingFileReadable());
+        assertTrue(isFileReadable(testingFile1));
+        assertTrue(isFileReadable(testingFile2));
 
     }
 
     @Test
-    public void testDifferentPoliciesFromMaximal() throws Exception {
+    public void testPolicySwitching() throws Exception {
 
-        assertTrue(isTestingFileReadable());
+        assertTrue(isFileReadable(testingFile1));
+        assertTrue(isFileReadable(testingFile2));
 
-        PolicyManager.INSTANCE.setPolicyFile(testingFileAllowingPolicy);
+        PolicyManager.INSTANCE.setPolicyFile(policyAllowingTestingFile1);
 
-        assertTrue(isTestingFileReadable());
+        assertTrue(isFileReadable(testingFile1));
+        assertFalse(isFileReadable(testingFile2));
 
-        PolicyManager.INSTANCE.setPolicyFile(minimalPolicy);
+        PolicyManager.INSTANCE.setPolicyFile(policyAllowingTestingFile2);
 
-        assertFalse(isTestingFileReadable());
+        assertFalse(isFileReadable(testingFile1));
+        assertTrue(isFileReadable(testingFile2));
 
         PolicyManager.INSTANCE.setPolicyFile(null);
 
-        assertTrue(isTestingFileReadable());
+        assertTrue(isFileReadable(testingFile1));
+        assertTrue(isFileReadable(testingFile2));
 
     }
 
-    public boolean isTestingFileReadable() throws IOException{
+    public boolean isFileReadable(File file) throws IOException{
         try{
-            FileInputStream is = new FileInputStream(testingFile);
+            FileInputStream is = new FileInputStream(file);
             is.read();
             is.close();
-            System.out.println("Testing file readable");
+            System.out.println("File "+file.getName()+" readable");
             return true;
         }
         catch(AccessControlException e){
-            System.out.println("Testing file not readable");
+            System.out.println("File "+file.getName()+" not readable");
             return false;
         }
     }
